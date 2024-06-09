e7387cc17d57b531dad3d7689f78bba317c544f1
Danim1981 <daniel.mendez.rodriguez@gmail.com>
2024-06-08 23:29:07 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Alert from './components/Alert'
+import Buscador from './components/Buscador'
+import Formulario from './components/Formulario'
+import Listado from './components/Listado'
+function App() {
+ 
+
+  return (
+    <>
+        
+       <Formulario/>
+       <Buscador/>
+       <Alert/> 
+       <Listado/>
+       
+     
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Listado.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Listado() {
+    return (
+        <div>
+          <h3>Listado lalalaa </h3>
+            </div>
+    )
+}
+
+export default Listado
