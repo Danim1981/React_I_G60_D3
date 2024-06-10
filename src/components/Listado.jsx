5f7bb3cb144a9230d568230fdd263e4f228ae21e
Danim1981 <daniel.mendez.rodriguez@gmail.com>
2024-06-10 17:56:06 -0400

    cambios.1

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { useState } from "react";
+import React from "react";
+import { baseColaboradores } from "../utils/BaseColaboradores";
 
-function Listado() {
+const Listado = () => {
+    const [nombreColaborador, setNombreColaborador] = useState("");
+    const [baseColaboradores, setBaseColaboradores] = useState(["colaborador 1", "colaborador 2", "colaborador3"]);
+
+   // const lista = listaColaboradores.map((colaborador) =>
+  //     colaborador.toUpperCase()
+   // );
+
+    const enviarFormulario = (e) => {
+        e.preventDefault();
+        setBaseColaboradores([...baseColaboradores, nombreColaborador]);
+        setNombreColaborador("");
+    };
+
+    const capturaInput = (e) => {setNombreColaborador(e.target.value)};
+
     return (
-        <div>
-          <h3>Listado lalalaa </h3>
-            </div>
-    )
+        <>
+            <form onSubmit={enviarFormulario}>
+                <input name="nombreColaborador" onChange={capturaInput} />
+                <button> Agregar tarea</button>
+            </form>
+            <ul>
+                {baseColaboradores.map((colaborador) => (
+                    <li key={id}>{colaborador}</li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
-export default Listado
+export default Listado;
--- src/utils/BaseColaboradores.js
+++ src/utils/BaseColaboradores.js
@@ -1,4 +1,4 @@
-export const BaseColaboradores = [
+export const baseColaboradores = [
     {
     id: "1",
     nombre: "Juan Soto",
